15b45c6eafa730a8d1294a43fc7aff48e046aaec
Yasir-Khan-7 <110191648+Yasir-Khan-7@users.noreply.github.com>
2024-05-14 18:27:37 +0500

    Update day3.py

--- day3.py
+++ day3.py
@@ -26,4 +26,29 @@ print(text2.endswith("***"))
 text2 = "Welcome to the console ***"
 print(text2.endswith('to',4,10))# check in between aswell
 
-#find() --> it finds the first occurance of the desired s tring
+#find() --> it finds the first occurance of the desired string and return its index if not present then returns -1
+heading = "Hello yasir is this and is that "
+print(heading.find('is'))
+
+#index()-->same as find() but if string is absent then raise an exception in output
+# print(heading.index("xyz"))
+
+#isalnum()-->this check if string is made up of alpha-numeric(A-Z,a-z,0-9) then retuns true otherwise false
+
+print(heading.isalnum())#--> gives false because of spacing
+
+#isalpha()-->checks if A-Z, a-z  is present then return true otherwise returns false
+
+text = 'yasir0'
+print(text.isalpha())#--> gives false because of  0 present
+
+#islower()--> returns true if all the characters are in lower case otherwise false
+
+a = 'sharjeel khan'
+print(a.islower())
+
+#isprintable()--> checks if all the characters in string is printable or not return true otherwise false
+b =" hello how are you"
+c=" hello how are yo \n iam good"
+print(b.isprintable())
+print(c.isprintable())
