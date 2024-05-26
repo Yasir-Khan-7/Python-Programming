0cebc42769cc05e0f14ef4b89383d8985706d1ac
Yasir-Khan-7 <110191648+Yasir-Khan-7@users.noreply.github.com>
2024-05-26 19:10:59 +0500

    Update day6.py

--- day6.py
+++ day6.py
@@ -65,3 +65,95 @@ C1 = {'peshawar','islamabad','lahore','karachi'}
 C2 = {'peshawar','islamabad'}
 C2.issubset(C1)
 
+ #if you want to add item to the set use add()
+C1.add('india')
+print(C1)
+
+#if you want to add more than one item use update()
+C3 = {'attock','Nowshera','mardan'}
+
+C1.update(C3)
+print(C1)
+
+#remove()/discard()
+#the difference between remove and discard is that the remove raises error if element not present while discard doesnot
+C1.remove('peshawar')
+print(C1)
+
+#u can pop() an element it will be removed from end but we donot which element will be present in last as sets are random
+#but can check that element by storing in variable
+item = C1.pop()
+print(item)
+
+#del is a keyword that delete entire dataset
+del C1
+print(C1) # it will raise error as C1 is deleted 
+
+# clear() is method that clear entire items in dataset and give empty set
+C2.clear()
+print(C2)
+
+#check if item exist in set
+info = {'yasirx',23,'BSSE'}
+if 'yasir' in info:
+    print(True)
+else:
+    print(False)
+    
+    
+#dictionary are ordered collection of item and these items are key value pairs
+studata = {'name': 'yasir', 'age':23, 'department':"BSSE","batch":"2024"}
+
+#accessing the items in the dictionary
+print(studata['name'])#it throws error if key is not present
+print(studata.get('name'))#it prints none if key is not present
+# both are used to access the data
+#if you want to access all the keys or values
+
+print(studata.keys())
+print(studata.values())
+
+#you can iterate each value aswell
+
+for key in studata.keys():
+     print(f"the value corresponding to {key} is {studata[key]}")
+
+#accessing keyvalue pairs
+print(studata.items())
+for key,value in studata.items():
+      print(f"the value corresponding to {key} is {value}")
+
+#dictionary methods
+#u can use update() to update your dictionary
+detail1 = {133:'Ammad',139:'yasir',121:"hamza"}
+details2= {110:'ahmed',138:'suffian',136:"farzam"}
+detail1.update(details2)
+print(detail1)
+#clear() it clears all items and gives empty dictionary
+details2.clear()
+print(details2)
+
+#use of pop() it takes key as parameter and removes key value pair
+detail1.pop(133)
+print(detail1)
+
+#popitem() it removes last keyvalue pair
+detail1.popitem()
+print(detail1)
+
+#use of del keyword which will delete entire dictionary or if you give parameter key than desired key value pair will be deleted
+del detail1[139]
+detail1
+# or u can delete entire dictionary
+del detail1
+
+print(detail1) # not defined error because it got deleted
+
+#using for loop with else --> else can be used with loop
+
+for i in range(2,10,2):
+    print(i)
+else:
+    print('sorry i is finished ')
+    
+#note it only executes if loop iteration is completed and if u put break statement it wonot executes
