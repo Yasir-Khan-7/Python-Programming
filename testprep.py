026ee618d5b719c51834c1000df6866720c6360e
Yasir-Khan-7 <110191648+Yasir-Khan-7@users.noreply.github.com>
2024-06-14 16:12:54 +0500

    Update testprep.py

---
+++ testprep.py
@@ -0,0 +1,44 @@
+#string are immutable
+#anything u enclose in double or single quote is called string
+name = 'yasir'
+friend ='sharjeel'
+#when u want to add double quote
+print("""hello 
+    jani kaisay ho
+    """)
+    #indexing in string
+print(name[0])
+
+#looping through string
+for i in name:
+    print(i)
+print(len(name))
+
+#methods in string
+
+#len()
+print(len(name))
+#touppercase()
+print(name.upper())#it creates a copy of it as string is immutable
+#now lower method
+print(name.lower())
+
+#strip method strip from all side
+d = 'hello!!!'
+print(d.strip("!"))
+
+#rstrip -->rstrip  only from end
+d = "!!!!hello!!!!"
+print(d.rstrip("!"))
+
+#replace()
+name = 'john'
+newname ='sharjeel'
+print(name.replace(name, newname))
+#split method
+name = 'yasir sharjeel'
+print(name.split(" "))
+
+#capitalize method
+
+print(name.capitalize())
