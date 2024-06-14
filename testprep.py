5d7a532f2a70201cae1e9fcf0042f49b20a26ec2
Yasir-Khan-7 <110191648+Yasir-Khan-7@users.noreply.github.com>
2024-06-14 19:11:39 +0500

    Update testprep.py

--- testprep.py
+++ testprep.py
@@ -35,19 +35,54 @@ print(d.rstrip("!"))
 name = 'john'
 newname ='sharjeel'
 print(name.replace(name, newname))
-#split method
-name = 'yasir sharjeel'
+#split method it splits the text
+name = 'yasir is sharjeel'
 print(name.split(" "))
 
-#capitalize method
+#capitalize method it capatalize the first letter
 
 print(name.capitalize())
 
-print(" banda kla putr bn jao")
+
 
 
 print('hello jani')
 #IT Center the text
 print(name.center(100))
 
+#endswith() returns true and false for checking
+print(name.endswith("@"))
+#now see this 
+print(name.endswith('to',2,8))#ending index will be checked 
+
+#find() method give index else if not found give -1
+
+print(name.find("is")) 
+
+#index() method is same like but gives exception on if not found
+print(name.index("jjgh"))
+
+#isalnum() give true if entire string only is Either CAPITAL A-Z or Small a-z or 0-9
+str = 'HELLO'
+str2 = '096'
+print(str.isalnum())
+print(str2.isalnum())
+
+#isalpha give true if entire string only is Either CAPITAL A-Z or Small a-z
+print(str.isalpha())
+print(str2.isalpha())#false because number not in isalpha
+
+#islower() checks whether all string are lower character and gives true if present otherwise false
+print(str.islower())#false because of capital
+
+#isupper() checks whether all string are upper character and gives true if present otherwise false
+print(str.isupper())#true because of upper
+
+#isprintable -- gives true if characters are printable means it doesnot include special characters
+str3 = 'hello world brother'
+str4 = "hello i am special \n ok"
+print(str3.isprintable())
+print(str4.isprintable())#false because of \n it is not printable character
+
+#ispace()
 
