36fcf077aec24bb4fd2a092b3e5be9a957f36d17
Yasir-Khan-7 <110191648+Yasir-Khan-7@users.noreply.github.com>
2024-06-18 20:56:03 +0500

    Update python_OOP.py

--- Yasir_work/python_OOP.py
+++ Yasir_work/python_OOP.py
@@ -17,7 +17,7 @@ print(book1.BookTitle)
 # Note: using _nameofattribute (single underscore) it means that the attribute is for internal use of class
 # Note: hasattr() is used to check if the attribute exists or not
 # Note: if you use double underscore before start of name of attribute or method
-# then python interpretor will change its so that other class cannot acces it
+# then python interpretor will change its so that other class cannot access it
 
 
 class Book:
@@ -26,6 +26,7 @@ class Book:
         self.author = author
         self.pages = pages
         self.price = price
+        self.__secret = "this is secret attribute"
 
     def getprice(self):
         if hasattr(self, "_discount"):
@@ -40,4 +41,30 @@ class Book:
 book3 = Book('high on', "james", 1239, 29.9)
 print(book3.getprice())
 book3.setdiscount(0.25)
+
 print(book3.getprice())
+
+# print(book3.__secret) # it will give error because python hides it
+# to check it use class name before u can then access itt
+print(book3._Book__secret)  # u will get the access check it
+
+
+
+#now using the type() function and isinstance() to check the type of instance
+
+class book:
+    def __init__(self,title):
+            self.title = title
+class newspaper:
+    def __init__(self,name):
+        self.name = name
+b1 = book("the Red line")
+b2 = book("cross over")
+n1 = newspaper("the dawn")
+
+n2 = newspaper("one news")
+
+#checking type
+print(type(b1))        
+print(type(n1))
+#comapring both types to check if they are same or not
