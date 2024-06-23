cda69f99af1073b041caac3c8bfef7123da0d2bd
Yasir-Khan-7 <yasirkhandps7@gmail.com>
2024-06-24 01:03:59 +0500

    Update extras.py

--- extras.py
+++ extras.py
@@ -8,3 +8,8 @@
 # 8) custom decorator in exceptional handling -- > using function
 # 9) learn about  custom exception + make bad argumentprogram( eg:notintargument)
 # 10) learn about multithreading and multiprocessing
+# 11) working with filess -- > opening , reading and writting
+# 12) working with csv format
+# 13) working with json format
+# 14) creating modules and pakages in python
+# 15) command line arguments for pakaging
