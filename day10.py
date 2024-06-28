510afec64a8c3d2282cf3a4f358ede57bd5c1b23
Yasir-Khan-7 <yasirkhandps7@gmail.com>
2024-06-28 17:48:49 +0500

    updated

--- day10.py
+++ day10.py
@@ -1,15 +1,15 @@
 import os
-# os.mkdir("data")#it will make a folder name data
+os.mkdir("data")#it will make a folder name data
 
-#now if i want to make 100 days of file in a folder name 'data'
+# now if i want to make 100 days of file in a folder name 'data'
 
-# for x in range(0,100):# this will create 100 folders with day 1 ,day 2 etc
-#     os.mkdir(f"data/day{x+1}")
+for x in range(0,100):# this will create 100 folders with day 1 ,day 2 etc
+    os.mkdir(f"data/day{x+1}")
     
-#if you want to rename the folders use rename()
+# if you want to rename the folders use rename()
 
-# for i in range(0,100):
-#     os.rename(f"data/day{i+1}",f"data/tutorial{i+1}") #first source and then destination
+for i in range(0,100):
+    os.rename(f"data/day{i+1}",f"data/tutorial{i+1}") #first source and then destination
     
 if not os.path.exists("data"):# twhen we want to check whether this already exists or not
     for x in range(0,100):# this will create 100 folders with day 1 ,day 2 etc
--- logicbuilding.py
+++ logicbuilding.py
@@ -1,37 +1,52 @@
+#Note: & in Python
+# The ‘&‘ symbol is a bitwise AND operator in Python, it is also known as a bitwise AND operator.
+# It operates on the bitwise representation of integers.
+
+# Note: The ‘and‘ keyword in Python is used in the logical operations. It is used to combine two logical statements, 
+# it returns TRUE if both are correct and FALSE if any of the statements is wrong.
+
 # write a program diivsible by 7 not multple of 5 from 2000 to 3200
-lst = []
-for i in range(2000, 3201):
-    if i % 7 == 0 & i % 5 == 0:
-        lst.append(i)
+lst =[]
+for number in range(2000,3200):
+
+    if number%7==0 and number%5!=1:
+        lst.append(number)
+
 print(lst)
 
 # write a program to calculate its factorial
 # fact of 0 and 1 is always 1
-
-
 def factorial(num):
-    if num == 0 or num == 1:
+    if num ==0 and 1:
         return 1
     else:
-        return num*factorial(num-1)
+       return num*factorial(num-1)
+res = factorial(5)
+print(f"The factorial of {5} is: {res}")
 
 
-factorial(4)
+# program 3 write a program that takes a number 
+# and creates dictionary of all previous numbers key is its index and square is its  value
 
-# program 3 write a program that takes a number and creates dictionary of all previous numbers key is its index and square is its  value
-
-
-def dictionary(num):
-    dic = {}
-    for i in range(1, num+1):
-        dic[i] = i*i
-    return dic
-
-
-print(dictionary(5))
-
+Dictionary={}
+def dict_number(num):
+    
+    for  x in range(1,num):
+     
+        Dictionary[x] = x*x
+        
+dict_number(5)    
+print(Dictionary)
 
 # check for prime factors
+lst2 = []
+def Prime(num):
+    
+        if num%num==0 and num%1==0:
+            lst2.append(num)
+       
+Prime()
+print(lst2)       
 # check for palindrome
 # sort a string
 # find all list items
@@ -47,5 +62,4 @@ print(dictionary(5))
 # build a zip archive
 # download sequential files
 
-print("hello world!")
-print("temporary")
+
